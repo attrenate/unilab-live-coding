a2cd5e62abc07944ab5a7d66e1762ee32e7a1f02
attrenate <nikolozghomidzevol100@gmail.com>
2025-05-11 15:11:45 +0400

    mark-up phase complete

---
+++ script.js
@@ -0,0 +1,17 @@
+// 3rd button
+const compareWords = document.querySelector('#words');
+compareWords.addEventListener('click', countWords());
+
+function countWords(string1, string2){}
+
+// 2nd button
+const characters = document.querySelector('#char');
+characters.addEventListener('click', countChar());
+
+function countChar(string1, string2){}
+
+// 1st button
+const match = document.querySelector('#match');
+match.addEventListener('click', matching())
+
+function matching(string1, string2){}
